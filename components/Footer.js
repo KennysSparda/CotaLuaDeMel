c7f15f93e19160ba273d484ffb573eef77d6bb04
swygax <kennykysv@gmail.com>
2022-09-14 20:11:39 -0300

    Added sky to footer copyright and corrected scripts in package.json

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-        <span>&copy; <strong>Swygax</strong> and <strong>Sky</strong></span>
+        <span>&copy; <strong>Swygax</strong> and <strong>Sky</strong>C</span>
     </footer>
   )
 }
