610f9c92fd32a599d0804746196bc71e5308bdbb
swygax <kennykysv@gmail.com>
2022-09-14 22:08:39 -0300

    Corrected nick of designer Sky*

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-        <span>&copy; <strong>Swygax</strong> and <strong>Sky</strong>C</span>
+        <span>&copy; <strong>Swygax</strong> and <strong>Sky</strong></span>
     </footer>
   )
 }
